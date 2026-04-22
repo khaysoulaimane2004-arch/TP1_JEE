9c2f05ee927b3f01c6848fd01ac6113751d853cf
khaysoulaimane2004-arch <kaysoulaimane2004@gmail.com>
2026-04-22 13:36:06 +0100

    Initial commit

---
+++ src/main/java/entities/Machine.java
@@ -0,0 +1,24 @@
+package entities;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Entity
+@NamedNativeQuery(name = "findBetweenDateNative", query = "select * from machine where dateAchat between :d1 and :d2", resultClass = Machine.class)
+@NamedQuery(name = "findBetweenDate", query = "from Machine where dateAchat between :d1 and :d2")
+public class Machine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String ref;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateAchat;
+
+    @ManyToOne
+    private Salle salle;
+
+    public Machine(){
+    }
+}
